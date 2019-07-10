a783354bd32f40de4be6f3379077e52d6f9e4f67
Hannah0124 <hannahjoo24@gmail.com>
2019-07-10 16:40:33 -0700

    Profile model, API and validation

---
+++ models/Profile.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+// Reference my users information. (This Profile is attached to the users)
+ user: {
+   // ObjectID means I’m reference another object.
+   type: Schema.Types.ObjectId,
+   ref: 'users'
+ },
+ handle: {  // userName
+  type: String,  
+  required: true,
+  max: 30
+},
+ photo: {
+   type: String
+ },
+ name: {
+   type: String
+ },
+ website: {
+   type: String
+ },
+ bio: {
+   type: String
+ },
+ privateInfo: {
+   email: {
+     type: String
+   },
+   phone: {
+     type: String
+   },
+   gender: {
+     stype: String
+   }
+ },
+ date: { 
+   type: Date,
+   default: Date.now
+ }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
+
